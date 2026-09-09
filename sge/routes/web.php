4d58a91d3b91c1af8358feea70126dd79d4dd761
MiguelAngelRamirezGutierrez <ramirezps3ps42015@gmail.com>
2026-09-09 11:46:57 -0500

    agregar logo

--- sge/routes/web.php
+++ sge/routes/web.php
@@ -1,10 +1,25 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
-
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // Módulo de Productos
+    Route::resource('products', ProductController::class)->names('products');
+    // Módulo de Categorías
+    Route::resource('categories', CategoryController::class)->names('categories');
+    // Módulo de Clientes
+    Route::resource('clients', ClientController::class)->names('clients');
+    // Módulo de Ventas
+    Route::resource('sales', SaleController::class)->names('sales');
+});
+require __DIR__ . '/auth.php';
