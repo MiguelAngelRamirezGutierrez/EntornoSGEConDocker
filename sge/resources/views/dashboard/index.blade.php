41c05af911ed13924010e69009c46d72bb1f94f9
MiguelAngelRamirezGutierrez <ramirezps3ps42015@gmail.com>
2026-09-09 14:04:41 -0500

    productController

--- sge/resources/views/dashboard/index.blade.php
+++ sge/resources/views/dashboard/index.blade.php
@@ -105,6 +105,7 @@
 @endsection
 
 @push('scripts')
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <script>
     document.addEventListener('DOMContentLoaded', function() {
         const ctx = document.getElementById('userChart').getContext('2d');
